4819c37b7b79b6337979f5530195a9d0a0ddcaa2
jcratebo703 <jcratebo703@gmail.com>
2020-05-24 14:20:18 +0800

    Hw01 finished

--- DataAnalysis.R
+++ DataAnalysis.R
@@ -27,26 +27,24 @@ Df107$SalaryIncreaseRatio = Df107$經常性薪資.薪資/Df104$經常性薪資.�
 Df107 = Df107[order(Df107$SalaryIncreaseRatio, decreasing = T),]
 
 #Q1
-Df107 %>%
+Df107sort = Df107 %>%
   filter(SalaryIncreaseRatio>1) %>%
-  select(大職業別) %>%
-  head(10)
+  select(大職業別,SalaryIncreaseRatio)
+  
+head(Df107sort,10)
 str(Df107)
 
 #Q2
-Df107 %>%
+Over5 = Df107 %>%
   filter(SalaryIncreaseRatio>1.05) %>%
   select(大職業別)
-str(Df107)
+
+head(Over5,10)
 
 #Q3
 Df107$大職業別 = as.character(Df107$大職業別)
 Df104$經常性薪資.薪資 = as.numeric(Df104$經常性薪資.薪資)
 str = strsplit(Df107$大職業別,"-")
-
-splitFunc =  function(){
-  
-}
 
 for(n in 1:nrow(Df107)){
   Df107$Category[n] = str[[n]][1]
@@ -55,6 +53,58 @@ for(n in 1:nrow(Df107)){
 
 table(Df107$Category)
 
+#Q4
+Df107$經常性薪資.女.男 = as.numeric(Df107$經常性薪資.女.男)
+Df104$經常性薪資.女.男 = as.numeric(Df104$經常性薪資.女.男)
 
+maleOrder107 = Df107 %>%
+  filter(經常性薪資.女.男<100) %>%
+  select(大職業別, 經常性薪資.女.男)
+
+maleOrder107 = maleOrder107[order(maleOrder107$經常性薪資.女.男),]
+maleOrder107
+
+maleOrder104 = Df104 %>%
+  filter(經常性薪資.女.男<100) %>%
+  select(大職業別, 經常性薪資.女.男)
+
+maleOrder104 = maleOrder104[order(maleOrder104$經常性薪資.女.男),]
+maleOrder104
+
+femaleOrder107 = Df107 %>%
+  filter(經常性薪資.女.男>100) %>%
+  select(大職業別, 經常性薪資.女.男)
+
+femaleOrder107 = femaleOrder107[order(femaleOrder107$經常性薪資.女.男, decreasing = T),]
+femaleOrder107
+
+femaleOrder104 = Df104 %>%
+  filter(經常性薪資.女.男>100) %>%
+  select(大職業別, 經常性薪資.女.男)
+
+femaleOrder104 = femaleOrder104[order(femaleOrder104$經常性薪資.女.男, decreasing = T),]
+femaleOrder104
+
+str(Df107)
+
+#Q5
+Df107$大學.薪資 = as.numeric(Df107$大學.薪資)
+Df107$研究所.薪資 = as.numeric(Df107$研究所.薪資)
+
+Df107$IncreaseFromDegree = Df107$研究所.薪資/Df107$大學.薪資
+ms107 = Df107 %>%
+  select(大職業別, 研究所.薪資, 大學.薪資, IncreaseFromDegree)
+
+ms107 = ms107[order(ms107$IncreaseFromDegree, decreasing = T),]
+ms107
+
+#Q6
+
+#不動產業-專業人員、金融及保險業-技術員及助理專業人員、工業-專業人員、製造業-事務支援人員、服務業-專業人員
+Df107$IncreaseAmount = Df107$研究所.薪資-Df107$大學.薪資
+position5 = Df107 %>%
+  select(大職業別, 研究所.薪資, 大學.薪資, IncreaseAmount) %>%
+  filter(大職業別 %in% c("不動產業-專業人員","金融及保險業-技術員及助理專業人員","工業-專業人員","製造業-事務支援人員","服務業-專業人員"))
+position5
 
 
